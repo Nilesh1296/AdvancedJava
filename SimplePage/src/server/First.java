99128eca520624992660498916b9b67d2f7def14
Nilesh1296 <singhnilesh151296@gmail.com>
2018-05-21 10:11:00 +0530

    push

---
+++ SimplePage/src/server/First.java
@@ -0,0 +1,21 @@
+package server;
+
+import java.io.IOException;
+
+
+/**
+ * Servlet implementation class First
+ */
+@WebServlet("/First")
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public First() {
+        // TODO Auto-generated constructor stub
+    }
+
+	
+}
